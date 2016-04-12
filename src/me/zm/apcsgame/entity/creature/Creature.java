91c35348ef1fbfb119a014c21e0e3fd22aff6cfd
Zachary Morris <ztowne13@gmail.com>
2016-04-12 10:37:18 -0700

    Added speed, health, size to creature.

--- src/me/zm/apcsgame/entity/creature/Creature.java
+++ src/me/zm/apcsgame/entity/creature/Creature.java
@@ -10,6 +10,8 @@ public abstract class Creature extends Entity
 {
 	CreatureType creatureType;
 
+	int health, size, speed;
+
 	public Creature(Game game, int x, int y, CreatureType creatureType)
 	{
 		super(game, x, y);
@@ -26,5 +28,35 @@ public abstract class Creature extends Entity
 	public void setCreatureType(CreatureType creatureType)
 	{
 		this.creatureType = creatureType;
+	}
+
+	public int getHealth()
+	{
+		return health;
+	}
+
+	public void setHealth(int health)
+	{
+		this.health = health;
+	}
+
+	public int getSize()
+	{
+		return size;
+	}
+
+	public void setSize(int size)
+	{
+		this.size = size;
+	}
+
+	public int getSpeed()
+	{
+		return speed;
+	}
+
+	public void setSpeed(int speed)
+	{
+		this.speed = speed;
 	}
 }
